21764d7136c73905f496e9b82e0789596558afd0
ChienTruong <qchien2400@gmail.com>
2018-04-08 18:57:48 +0700

    08/08/18

--- src/main/java/wordnet/Util/PathFile.java
+++ src/main/java/wordnet/Util/PathFile.java
@@ -5,12 +5,12 @@ package wordnet.Util;
  */
 public class PathFile {
 
-    public static final String indexFile = "file/index.noun";
-    public static final String dataFile = "file/data.noun";
-    public static final String fileDataEV = "file/ev.txt";
-    public static final String fileSynonym = "file/synDict.txt";
-    public static final String fileSpecial = "file/special.txt";
-    public static final String exportExcel = "file/output.xlsx";
+    public static final String indexFile = "/home/chien/Documents/WordNet/index.noun";
+    public static final String dataFile = "/home/chien/Documents/WordNet/data.noun";
+    public static final String fileDataEV = "/home/chien/Documents/WordNet/ev.txt";
+    public static final String fileSynonym = "/home/chien/Documents/WordNet/synDict.txt";
+    public static final String fileSpecial = "/home/chien/Documents/WordNet/specialC.txt";
+    public static final String exportExcel = "/home/chien/Documents/WordNet/output.xlsx";
 
 
 }
